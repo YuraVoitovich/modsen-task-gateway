e0d6d613b4906d761a9c097c0d73bed0914afd94
voito <voitovich200233@gmail.com>
2023-09-26 15:48:04 +0300

    Initial commit

---
+++ src/main/java/io/voitovoich/testmodsentask/modsentaskgateway/ModsenTaskGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.voitovoich.testmodsentask.modsentaskgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsenTaskGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModsenTaskGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/voitovoich/testmodsentask/modsentaskgateway/ModsenTaskGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.voitovoich.testmodsentask.modsentaskgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModsenTaskGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
